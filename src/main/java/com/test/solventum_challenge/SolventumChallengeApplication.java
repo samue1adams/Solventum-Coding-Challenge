66a232b0e7a1590977e388d2a609a294ca528e59
Sam Adams <samadams@Mac.attlocal.net>
2025-05-01 15:23:23 -0500

    initial commit

---
+++ src/main/java/com/test/solventum_challenge/SolventumChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.test.solventum_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolventumChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolventumChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/solventum_challenge/SolventumChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.solventum_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolventumChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
